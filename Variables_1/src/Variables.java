d1369ce2b1d6118671d70e5bd4bab42fa81b6b41
davidaltamiranodev <davidaltamiranodev@gmail.com>
2022-11-17 07:32:30 -0500

    Course JAVA SE - Variables_1

---
+++ Variables_1/src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declaración de Variables
+        int speed;
+        // Asignamos un valor a una variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Pedro Perez";
+        System.out.println(employeeName);
+    }
+}
