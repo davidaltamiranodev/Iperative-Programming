0f979e81f6617ec201067ca65454a05fc827fe9c
davidaltamiranodev <davidaltamiranodev@gmail.com>
2022-11-29 05:55:14 -0500

    Course JAVA SE - LogicOperations

---
+++ LogicOperations/src/LogicOperations.java
@@ -0,0 +1,12 @@
+public class LogicOperations {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+
+        if (a == b) {
+            System.out.println("a es igual a b");
+        }else if ((a != b) && (a > b)){
+            System.out.println("a es diferente de b");;
+        }
+    }
+}
